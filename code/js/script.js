e56ff2dea5d010495cf95407f321248f7bf642cf
Resto110 <francesco.infoandjobs@gmail.com>
2025-09-08 05:27:20 +0200

    login page completed

--- code/js/script.js
+++ code/js/script.js
@@ -3,5 +3,23 @@ const form = document.getElementById("login");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
+    const user = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    console.log("Username:", user);
+    console.log("Password:", password);
+
+    if (user === "") {
+        alert("Please enter username.");
+        return;
+    }
+
+    if (password === "") {
+        alert("Please enter password.");
+        return;
+    }
+
+    // if both fields filled
+    alert("Checking your data and logging in...");
     location.reload();
 });
